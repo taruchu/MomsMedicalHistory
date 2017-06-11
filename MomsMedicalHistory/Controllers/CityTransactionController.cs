adbad3dce1f51c8e533e35f453a25c746312d925
taruchu <tmccullough.softwaredeveloper@gmail.com>
2017-06-11 12:54:44 -0700

    .NET MVC project for Medical History
    Hello :=)
    
    Info: visual studio 2015 MVC project using .NET 4.6.2 and Entity
    Framwork version 6.0.0.0.
    
    Description:
    
    This is currently an ongoing project I have with my mother. She needs a
    medical history CRUD application, with one exception,
    no deleting. This is because she wants to keep a long term history
    record of all the information. The model is straight forward,
    and the UI will provide access to lists, data entry, and reports. Some
    other future plans are as follows:
    (1) Create a phone app that will, for each report, synch/download latest
    report data in JSON using a .NET MVC Web API REST entry point.
    (2) Create a UI feature that can push doctor appointments to her outlook
    using "Outlook Push Notifications REST API".
    
    One important note is that this application is not public. It will
    reside on a home intranet and she will be the only user.
    
    There is a backup of the database in the folder Database.
    
    ------ Taruchu :=)

---
+++ MomsMedicalHistory/Controllers/CityTransactionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MomsMedicalHistory.Models;
+ 
+
+namespace MomsMedicalHistory.Controllers
+{
+     
+    public class CityTransactionController : Controller
+    {
+        public ActionResult CityList()
+        {
+            IEnumerable<city> model = Medical_Machine_Routeens
+                                       .Read_city();
+            return View("CityList", model);
+        }
+
+        public ActionResult CityListRefresh()
+        {
+            IEnumerable<city> model = Medical_Machine_Routeens
+                                        .Read_city();
+            return PartialView("CityListPartial", model);
+        }
+        public ActionResult ViewCity(int id)
+        {
+            city model =  Medical_Machine_Routeens
+                            .Read_city(id)
+                            .FirstOrDefault<city>();
+            return PartialView("City", model);
+        }
+
+        public ActionResult ExecuteCityTransaction(city model)
+        {
+            if (model.city_ID > 0) 
+                Medical_Machine_Routeens.Update_city(model);       
+            else 
+                Medical_Machine_Routeens.Create_city(model); 
+                 
+            return RedirectToAction("ViewCity", new { id = model.city_ID });
+        }
+ 
+    }
+}
---
+++ MomsMedicalHistory/Models/Routeens/Medical_Machine_Routeens.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Transactions;
+using System.Data.SqlClient;
+
+
+namespace MomsMedicalHistory.Models
+{
+    public class Medical_Machine_Routeens
+    {
+        public static IEnumerable<allergic_medications> Read_allergic_medications(int? id = null)
+        {
+            IEnumerable<allergic_medications> query = null;
+            using (TransactionScope tran = new TransactionScope())
+            {
+                try
+                {
+                    using(Medical_MachineEntities context = new Medical_MachineEntities())
+                    {
+                        
+                        query = context
+                            .allergic_medications
+                            .ToList<allergic_medications>();
+                        query = (id == null) ?
+                                        query
+                                        : query
+                                            .Where(a => a.allergic_ID == id);                                                    
+                    }
+                    tran.Complete();
+                    tran.Dispose();
+                }
+                catch(TransactionException ex)
+                {
+                    tran.Dispose();
+                    throw new ApplicationException(ex.Message);
+                }
+                catch(SqlException ex)
+                {
+                    throw new ApplicationException(ex.Message);
+                }
+                return query;
+            }
+        }
+        public static void Create_allergic_medications(allergic_medications model)
+        {
+            if(model != null)
+                using(TransactionScope tran = new TransactionScope())
+                {
+                    try
+                    {
+                        using(Medical_MachineEntities context = new Medical_MachineEntities())
+                        {
+                            context.allergic_medications.Add(model);
+                            context.SaveChanges(); 
+                        }
+                        tran.Complete();
+                        tran.Dispose();
+                    }
+                    catch(TransactionException ex)
+                    {
+                        tran.Dispose();
+                        throw new ApplicationException(ex.Message);
+                    }
+                    catch(SqlException ex)
+                    { 
+                        throw new ApplicationException(ex.Message);
+                    }
+                }
+        }
+        public static void Update_allergic_medications(allergic_medications model)
+        {
+            if(model != null)
+                using(TransactionScope tran = new TransactionScope())
+                {
+                    try
+                    {
+                        using(Medical_MachineEntities context = new Medical_MachineEntities())
+                        {
+                            context
+                              .allergic_medications
+                              .ToList<allergic_medications>()
+                              .Where(a => a.allergic_ID == model.allergic_ID)
+                              .FirstOrDefault<allergic_medications>()
+                              .allergic_name = model.allergic_name;
+                            context.SaveChanges();                           
+                        }
+                        tran.Complete();
+                        tran.Dispose();
+                    }
+                    catch(TransactionException ex)
+                    {
+                        tran.Dispose();
+                        throw new ApplicationException(ex.Message);
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw new ApplicationException(ex.Message);
+                    }
+                }
+        }
+
+
+        public static IEnumerable<city> Read_city(int? id = null)
+        {
+            IEnumerable<city> query = null;
+            using (TransactionScope tran = new TransactionScope())
+            {
+                try
+                {
+                    using(Medical_MachineEntities context = new Medical_MachineEntities())
+                    {
+                        query = context
+                            .cities
+                            .ToList<city>();
+                        query = (id == null) ?
+                            query :
+                            query
+                                .Where(a => a.city_ID == id);
+                    }
+                    tran.Complete();
+                    tran.Dispose();
+                }
+                catch(TransactionException ex)
+                {
+                    tran.Dispose();
+                    throw new ApplicationException(ex.Message);
+                }
+            }
+            return query;
+        }
+        public static void Create_city(city model)
+        { 
+            if(model != null)
+                using(TransactionScope tran = new TransactionScope())
+                {
+                    try
+                    {
+                        using(Medical_MachineEntities context = new Medical_MachineEntities())
+                        {
+                            context.cities.Add(model);
+                            context.SaveChanges();                            
+                        }
+                        tran.Complete();
+                        tran.Dispose();
+                    }
+                    catch(TransactionException ex)
+                    {
+                        tran.Dispose();
+                        throw new ApplicationException(ex.Message);
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw new ApplicationException(ex.Message);
+                    }
+                } 
+        }
+        public static void Update_city(city model)
+        {
+            if(model != null)
+                using (TransactionScope tran = new TransactionScope())
+                {
+                    try
+                    {
+                        using (Medical_MachineEntities context = new Medical_MachineEntities())
+                        {
+                            context
+                                .cities
+                                .ToList<city>()
+                                .Where(a => a.city_ID == model.city_ID)
+                                .FirstOrDefault<city>()
+                                .city_name = model.city_name;
+                            context.SaveChanges();                            
+                        }
+                        tran.Complete();
+                        tran.Dispose();
+                    }
+                    catch(TransactionException ex)
+                    {
+                        tran.Dispose();
+                        throw new ApplicationException(ex.Message);
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw new ApplicationException(ex.Message);
+                    }
+                }
+        }
+
+
+
+
+    }
+}
